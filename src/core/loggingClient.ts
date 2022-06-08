67febdfb67c3350db858220a2bbe6592280126a1
ReallyLiri <reallyliri@gmail.com>
2022-06-08 18:54:00 +0300

    fixed paging

--- src/core/loggingClient.ts
+++ src/core/loggingClient.ts
@@ -50,27 +50,42 @@ const escapeQuery = (query: string) =>
 const toWebUrl = (projectId: string, query: string) =>
   `https://console.cloud.google.com/logs/query;query=${ escapeQuery(query) };?project=${ projectId }`;
 
-export async function readLogsPageAsync(request: FetchPageMessage): Promise<PageResultMessage> {
+let firstPageRequest: GetEntriesRequest | null = null;
+
+function buildRequest(fetchPage: FetchPageMessage): GetEntriesRequest | null {
+  if (fetchPage.pageToken) {
+    if (!firstPageRequest) {
+      console.error("Non first page requests must have a page token");
+      return null;
+    }
+    return {...firstPageRequest, pageToken: fetchPage.pageToken};
+  }
+  const request = {
+    filter: buildFilterText(fetchPage.filter),
+    resourceNames: [`projects/${ fetchPage.filter.projectId! }`],
+    orderBy: "timestamp desc",
+    pageSize: fetchPage.pageSize,
+  };
+  firstPageRequest = request;
+  return request;
+}
+
+export async function readLogsPageAsync(fetchPage: FetchPageMessage): Promise<PageResultMessage> {
   const result: PageResultMessage = {type: MessageType.PAGE_RESULT, entries: [], nextPageToken: null, webUrl: ""};
   try {
-    const projectId = request.filter.projectId!;
-    const requestBody: GetEntriesRequest = {
-      filter: buildFilterText(request.filter),
-      resourceNames: [`projects/${ projectId }`],
-      orderBy: "timestamp desc",
-      pageSize: request.pageSize,
-      pageToken: request.pageToken ?? undefined,
-    };
-    result.webUrl = toWebUrl(projectId, requestBody.filter!);
-    const entriesResponse = await httpAsync<GetEntriesRequest, IListLogEntriesResponse>(
-      'https://logging.googleapis.com/v2/entries:list?alt=json',
-      'post',
-      requestBody
-    );
+    const request = await buildRequest(fetchPage);
+    if (request) {
+      result.webUrl = toWebUrl(fetchPage.filter.projectId!, request.filter!);
+      const entriesResponse = await httpAsync<GetEntriesRequest, IListLogEntriesResponse>(
+        'https://logging.googleapis.com/v2/entries:list?alt=json',
+        'post',
+        request
+      );
 
-    if (entriesResponse) {
-      result.entries = entriesResponse.entries || [];
-      result.nextPageToken = result.entries.length === 0 ? null : entriesResponse.nextPageToken || null;
+      if (entriesResponse) {
+        result.entries = entriesResponse.entries || [];
+        result.nextPageToken = result.entries.length === 0 ? null : entriesResponse.nextPageToken || null;
+      }
     }
   } catch (e) {
     console.error("failed fetching logs", e);
