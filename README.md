a49727c5bebaea97e4a2caa7fe3e6e0556f2ee04
ReallyLiri <reallyliri@gmail.com>
2022-06-09 16:49:56 +0300

    gif

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Googlogs
 
-![icon](icon.png)
+VSCode extension to view Google Cloud Logs.
+
+![icon](res/icon.png)
+
+![demo](res/demo.gif)
+
+## Development
+
+`yarn browser` to run app in browser with data mocks.
+
+Lanuch from vscode to debug.
+
+`yarn vsix` to build `.vsix` file. `yarn vsix-install` to install the extension to your local vscode.
--- app/data/schema.ts
+++ app/data/schema.ts
@@ -3,9 +3,19 @@ import * as moment from "moment";
 import * as objectPath from "object-path";
 import ILogEntry = google.logging.v2.ILogEntry;
 
+const OBFUSCATE = false;
+
 const propertyRegexp = new RegExp("(\\.[a-z]+)+", "gis");
 
 export type EntryFormatter = (entry: ILogEntry) => string;
+
+function obfuscate(char: string) {
+  let charCode = char.charCodeAt(0);
+  if (charCode >= 97 && charCode <= 122) {
+    charCode = 97 + (charCode + Math.random() * 26) % 26;
+  }
+  return String.fromCharCode(charCode);
+}
 
 export function buildFormatter(schema: string): EntryFormatter {
   let pathToLiteral: [string, string][] = [];
@@ -29,6 +39,10 @@ export function buildFormatter(schema: string): EntryFormatter {
         } else {
           stringValue = rawValue.toString();
         }
+      }
+      if (OBFUSCATE) {
+        // for recording purposes
+        stringValue = stringValue.split("").map(c => obfuscate(c)).join("");
       }
       result = result.replace(literal, stringValue);
     }
